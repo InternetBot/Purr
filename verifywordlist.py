731937d61ea37c80af711749f1b54375038cf318
Calvin <calvincaleb1003@gmail.com>
2025-03-18 13:03:57 -0400

    Added more word list option

--- verifywordlist.py
+++ verifywordlist.py
@@ -4,10 +4,19 @@ def verify_wordlist(word_list):
     '''
     Checking if wordlist is empty if it is, sets this as default
     '''
-    if not word_list:
+    if not word_list.strip():
         print("⚠️ No wordlist provided! Using default: /usr/share/wordlists/")
-        return "/usr/share/wordlists/dirbuster/directory-list-2.3-small.txt "
 
+        if usr_wordlist_option == "s":
+            return "/usr/share/wordlists/dirbuster/directory-list-2.3-small.txt"
+        elif usr_wordlist_option == "m":
+            return "/usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt"
+        elif usr_wordlist_option == "l":
+            return "/usr/share/wordlists/dirbuster/directory-list-1.0.txt"
+        else:
+            print("⚠️ Invalid choice, defaulting to small.")
+            return"/usr/share/wordlists/dirbuster/directory-list-2.3-small.txt"
+            
     #to do add drop down for user to get more options of default wordlist
     #test word list /usr/share/wordlists/dirbuster/directory-list-2.3-small.txt 
 
