ecef12df8969d744562f07b7ac10ea69d4e05629
Calvin <calvincaleb1003@gmail.com>
2025-03-18 13:08:54 -0400

    Added a basic bash command that needs to be executed before the script run

---
+++ basic.sh
@@ -0,0 +1,9 @@
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y ffuf wordlists
+ls /usr/share/wordlists/dirbuster/
+sudo apt install --reinstall wordlists -y
+sudo apt install -y python3 python3-pip
+python3 main.py
+
+## more logic behind this to come 
+## just listing commmands that are in my head rn 
